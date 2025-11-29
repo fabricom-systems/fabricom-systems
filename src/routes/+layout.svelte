12cf410b0f29c3786c860bbc30300624b0372283
Pedro A. Fabri <pedroafabri@gmail.com>
2025-11-29 14:57:31 -0300

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import '98.css'
+	import '$lib/style/global.css'
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Fabricom Systems</title>
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { draggable } from "@neodrag/svelte";
+
+    let dragOpts = {
+        handle: '.title-bar',
+        bounds: 'body'
+    }
+</script>
+
+<div class="main-container">
+    <div class="window" style="width: 300px; height: 60px;" use:draggable={dragOpts}>
+        <div class="title-bar">
+            <div class="title-bar-text">Fabricom Systems</div>
+            <div class="title-bar-controls">
+            </div>
+        </div>
+        <div class="window-body">
+            <p>Coming soon!</p>
+        </div>
+    </div>
+</div>
+
+<style>
+    .main-container{
+        display: flex;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
