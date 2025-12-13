a847a00b188e1372792ef1d65fb0853405373e96
Pedro A. Fabri <pedroafabri@gmail.com>
2025-12-12 21:19:13 -0300

    chore: Added script to scaffold new blog posts

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ bun install
 bun run dev
 ```
 
+## Scripts
+
+```
+bun run new-post -> Creates a new blog post
+```
+
 ## TODO:
 
 [X] Add blog page listing
@@ -17,5 +23,6 @@ bun run dev
 [X] Order by publication date desc
 [X] Pre-commit to check for unique post slug & id
 [X] Refactor types for different file
-[ ] Create script to generate new post with metadata
+[X] Create script to generate new post with metadata
+[ ] Order posts by ID desc
 [ ] Delete test posts
---
+++ scripts/new-post.ts
@@ -0,0 +1,74 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import prompts from 'prompts'
+
+const POSTS_DIR = path.resolve(process.cwd(), 'src/posts');
+
+const slugify = (text: string) => {
+  return text
+    .toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-');
+};
+
+const getId = async () => {
+    const files = fs.readdirSync(POSTS_DIR);
+    return (files?.length ?? 0) + 1;
+}
+
+async function createPost() {
+    const response = await prompts([
+        {
+            type: 'text',
+            name: 'title',
+            message: "What is the title of the post?",
+            validate: (value) => value.length > 0 ? true : 'Title is required!'
+        },
+        {
+            type: 'text',
+            name: 'slug',
+            message: 'What is the slug?',
+            initial: (prev) => slugify(prev),
+            validate: (value) => {
+                if (fs.existsSync(path.join(POSTS_DIR, `${value}.md`))) {
+                    return 'Slug already taken!';
+                }
+                return true;
+            }
+        }
+    ]);
+
+    if (!response.title || !response.slug) return;
+    const { title, slug } = response;
+    const date = new Date().toLocaleDateString("en-CA").split('T')[0]; // en-CA uses YYYY-MM-DD
+    const id = await getId();
+
+    const lines = [
+        '---',
+        `id: ${id}`,
+        `title: "${title}"`,
+        `date: "${date}"`,
+        '---',
+        '',
+        `# ${title}`,
+        '',
+        '> [YOUR POST HERE]'
+    ]
+
+    const content = lines.join("\n");
+    const filePath = path.join(POSTS_DIR, `${slug}.md`);
+
+    try {
+        fs.writeFileSync(filePath, content);
+        console.log(`\n✅ Post created!`);
+    } catch(error) {
+        console.error('❌ Error creating post:', error);
+    }
+}
+
+createPost();
